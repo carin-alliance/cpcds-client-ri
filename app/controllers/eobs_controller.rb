609006495cd5a55a0988ec411d388d079328c358
Saul Kravitz <skravitz@mitre.org>
2020-02-14 17:05:04 -0500

    current

--- app/controllers/eobs_controller.rb
+++ app/controllers/eobs_controller.rb
@@ -2,16 +2,16 @@ class EobsController < ApplicationController
   # GET /eobs 
   def index # read a bundle from a file 
     load_bundle 
-    @eobs = explanationofbenefits.map { |eob| EOB.new(eob, practitioners, claims, locations, observations, nil) } 
+    @eobs = explanationofbenefits.map { |eob| EOB.new(eob, practitioners, claims, locations, observations, nil) }.sort_by { |a|  -a.sortDate }
     #binding.pry 
   end
   # GET /eobs/[id] 
   def show 
     load_bundle 
-    @eobs = explanationofbenefits.map { |eob| EOB.new(eob, practitioners, claims, locations, observations,nil) }.sort_by { |a|  -a.sortDate }
+    @eobs = explanationofbenefits.map { |eob| EOB.new(eob, practitioners, claims, locations, observations,nil) }
     #binding.pry # How do I get the id from the URL? id = 0; binding.pry reference = id.gsub("u rn:uuid:", "") 
     reference = params[:id]
-    @eob = @eobs.select{|p| p.id = reference}[0] 
     binding.pry 
+    @eob = @eobs.select{|p| p.id == reference}[0] 
   end
 end
