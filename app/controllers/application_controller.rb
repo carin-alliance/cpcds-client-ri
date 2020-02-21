db75404e53789a072ddbdf6bd3e8865234649c2f
Saul Kravitz <saul@mitre.org>
2020-02-20 22:22:18 -0500

    end of day Thursday:  eob index/show working using server data.  Home screen still not working.  Unclear why.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,11 +7,80 @@
 ################################################################################
 
 class ApplicationController < ActionController::Base
-    attr_accessor :explanationofbenefits, :practitioners, :patients, :locations, :organizations, :practitionerroles, :encounters,
+    before_action :establish_session_handler, only: [ :index, :show ]
+
+    attr_accessor :explanationofbenefits, :practitioners, :patients, :locations, :organizations, :practitionerroles, :encounters, :fhir_encounters,
     :observations, :procedures, :immunizations, :diagnosticreports, :documentreferences, :claims, :conditions, :medicationrequests,
     :careteams, :careplans, :devices, :provenances, :resources
+    attr_accessor :fhir_explanationofbenefits, 
+    :fhir_practitioners, :fhir_patients, :fhir_locations, :fhir_organizations, :fhir_practitionerroles, :fhir_encounters, :fhir_encounters,
+    :fhir_observations, :fhir_procedures, :fhir_immunizations, :fhir_diagnosticreports, :fhir_documentreferences, :fhir_claims, :fhir_conditions, :fhir_medicationrequests,
+    :fhir_careteams, :fhir_careplans, :fhir_devices, :fhir_provenances, :patient_resources
 
-   def load_bundle
+        CPCDS_PATIENT_PROFILE_URL = 'http://hl7.org/fhir/us/core/StructureDefinition/us-core-patient'
+        CPCDS_ENCOUNTER_PROFILE_URL = 'http://hl7.org/fhir/us/core/StructureDefinition/us-core-encounter'
+        CPCDS_EOB_PROFILE_URL = 'http://hl7.org/fhir/us/davinci-pdex-plan-net/StructureDefinition/plannet-InsurancePlan'
+        CPCDS_ENCOUNTER_PROFILE_URL = 'http://hl7.org/fhir/us/davinci-pdex-plan-net/StructureDefinition/plannet-Network'
+        CPCDS_PRACTITIONER_PROFILE_URL = 'http://hl7.org/fhir/us/davinci-pdex-plan-net/StructureDefinition/plannet-Practitioner'
+        CPCDS_ORGANIZATION_PROFILE_URL = 'http://hl7.org/fhir/us/davinci-pdex-plan-net/StructureDefinition/plannet-PractitionerRole'
+             
+
+
+    def load_patient_specific_data_from_server
+        # read all patient data from server
+        pid = session[:patient_id]
+        binding.pry if pid == nil 
+        @fhir_procedures = @fhir_procedures || load_patient_resources(FHIR::Procedure, nil, :subject, pid )
+        @fhir_encounters = @fhir_encounters || load_patient_resources(FHIR::Encounter, nil, :subject, pid )
+        @fhir_claims = @fhir_claims || load_patient_resources(FHIR::Claim, nil, :patient, pid )
+        @fhir_explanationofbenefits = @fhir_explanationofbenefits || load_patient_resources(FHIR::ExplanationOfBenefit, nil, :patient, pid )
+        @fhir_observations = @fhir_observations || load_patient_resources(FHIR::Observation, nil, :subject, pid )
+        @fhir_immunizations = @fhir_immunizations || load_patient_resources(FHIR::Immunization, nil, :patient, pid )
+        @fhir_conditions = @fhir_conditions || load_patient_resources(FHIR::Condition, nil, :subject, pid )
+        @fhir_diagnosticreports = @fhir_diagnosticreports || load_patient_resources(FHIR::DiagnosticReport, nil, :subject, pid )
+        @fhir_medicationrequests = @fhir_medicationrequests || load_patient_resources(FHIR::MedicationRequest, nil, :subject, pid )
+        @fhir_careteams = @fhir_careteams || load_patient_resources(FHIR::CareTeam, nil, :subject, pid )
+        @fhir_devices = @fhir_careteams || load_patient_resources(FHIR::Device, nil, :subject, pid )
+        binding.pry 
+        #@locations = nil # ||= @bundle.entry.select { |entry| entry.resource.instance_of? FHIR::Location }.map(&:resource)
+        #@organizations = nil # ||= @bundle.entry.select { |entry| entry.resource.instance_of? FHIR::Organization }.map(&:resource)
+        #@practitioners nil # ||= @bundle.entry.select { |entry| entry.resource.instance_of? FHIR::Practitioner }.map(&:resource)
+        #@practitionerroles nil # ||= @bundle.entry.select { |entry| entry.resource.instance_of? FHIR::PractitionerRole }.map(&:resource)
+      @resources = {
+         :patient => @fhir_patients,
+  #      :locations => @locations,
+  #      :organizations => @organizations,
+  #      :practitioners => @practitioners,
+  #      :practitionerroles => @practitionerroles,
+        :encounters => @fhir_encounters,
+        :observations => @fhir_observations,
+        :procedures => @fhir_procedures,
+        :immunizations => @fhir_immunizations,
+        :diagnosticreports => @fhir_diagnosticreports,
+        :documentreferences => @fhir_documentreferences,
+        :claims => @fhir_claims,
+        :observations => @fhir_observations,
+        :explanationofbenefits => @fhir_explanationofbenefits,
+        :conditions => @fhir_conditions,
+        :medicationrequests => @fhir_medicationrequests,
+        :careteams => @fhir_careteams,
+        :careplans => @fhir_careplans,
+        :devices => @fhir_devices,
+        }
+       end
+    
+       def load_patient_resources (type, profile, field, pid)
+        parameters = {}
+        parameters[field] = "Patient/" + pid
+        parameters[:_profile] = profile if profile
+        parameters[:_count] = 1000 
+        search = {parameters: parameters }
+        results = @client.search(type, search: search )
+        results.resource.entry.map(&:resource)
+       end
+
+       
+    def load_bundle
     # read a bundle from a file
     file = File.read('../cpcds-server-ri/CPCDS_patient_data/Darin74_Olson653.json')
     @bundle = FHIR::Json.from_json(file)
@@ -74,5 +143,71 @@ class ApplicationController < ActionController::Base
      def cleanup(id)
         id.gsub("urn:uuid:", "")
      end
+# Get the FHIR server url
+def server_url
+    params[:server_url] || session[:server_url]
+  end
+  def patient_id
+    params[:patient_id] || session[:patient_id]
+  end
+  def patient_key
+    params[:patient_key] || session[:patient_key]
+  end
 
+
+  # Connect the FHIR client with the specified server and save the connection
+  # for future requests.
+
+  def connect_to_server
+    if server_url.present?
+      @client = FHIR::Client.new(server_url)
+      @client.use_r4
+      @client.additional_headers = { 'Accept-Encoding' => 'identity' }  # 
+      cookies[:server_url] = server_url
+      session[:server_url] = server_url      
+     end
+   rescue => exception
+       err = "Connection failed: Ensure provided url points to a valid FHIR server"
+       redirect_to root_path, flash: { error: err }
+ 
+  end
+#-----------------------------------------------------------------------------
+  private
+  #-----------------------------------------------------------------------------
+  
+  def establish_session_handler
+    binding.pry 
+    if server_url.present? && patient_id.present? && patient_key.present?
+      session[:wakeupsession] = "ok" # using session hash prompts rails session to load
+      #SessionHandler.establish(session.id, params[:server_url])
+       binding.pry 
+    else 
+        err = "Please enter a FHIR server address, a valid patient ID, and a valid patient key."
+        binding.pry 
+        cookies[:session_url] = nil
+        session[:patient_key] = nil
+        cookies[:patient_id] = nil
+        session[:patient_id] = nil
+        redirect_to root_path, flash: { error: err }
+        return
+    end 
+      @client = FHIR::Client.new(server_url)
+      @client.use_r4
+      @client.set_bearer_token(patient_key)
+
+      # profile = 'http://hl7.org/fhir/us/carin/StructureDefinition/carin-bb-patient'
+      profile = 'http://hl7.org/fhir/us/core/StructureDefinition/us-core-patient'
+      search = { parameters: { _profile: profile,  _id: patient_id}, created: "ge2016-01-01"}
+      results = @client.search(FHIR::Patient, search: search )
+      binding.pry
+      raise 'Serious Error -- retrieved patient has wrong ID'  unless patient_id == results.resource.entry[0].resource.id 
+      @fhir_patients = results.resource.entry.map(&:resource)
+      cookies[:patient_key] = patient_key
+      session[:patient_key] = patient_key
+      cookies[:patient_id] = patient_id
+      session[:patient_id] = patient_id 
+      cookies[:server_url] = server_url
+      session[:server_url] = server_url 
+     
+    end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -163,5 +163,10 @@ module ApplicationHelper
 
 		raw(list.join(', '))
 	end
+
+	def hinted_text_field_tag(name, value = nil, hint = "Click and enter text", options={})
+		value = value.nil? ? hint : value
+		text_field_tag name, value, {:onclick => "if($(this).value == '#{hint}'){$(this).value = ''}", :onblur => "if($(this).value == ''){$(this).value = '#{hint}'}" }.update(options.stringify_keys)
+	end
 	
 end
