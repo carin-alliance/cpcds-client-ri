a361e968c1b8ba4ea8077cc6a19cd550da8637cb
Saul Kravitz <saul@mitre.org>
2020-03-04 16:49:53 -0500

    state  as of 3/4

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -9,7 +9,7 @@
 class DashboardController < ApplicationController
   before_action :establish_session_handler, only: [ :index, :show ]
   def index
-     load_patient_specific_data_from_server
+  #   load_patient_specific_data_from_server
     # sets up @patient
     @patient = Patient.new(@fhir_patients[0], @resources, @client)
   end
