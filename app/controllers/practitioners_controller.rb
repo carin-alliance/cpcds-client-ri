cb5afc37a4d06c4ba3a98535d587fee8665a0ecf
Hill, Dave <dwhill@mitre.org>
2020-11-17 21:53:04 -0500

    Fixed comment syntax.

--- app/controllers/practitioners_controller.rb
+++ app/controllers/practitioners_controller.rb
@@ -9,7 +9,7 @@
 class PractitionersController < ApplicationController
 
 	def show
-		/* This search should be from resources included in EOB search */
+	  # This search should be from resources included in EOB search
 		fhir_client = SessionHandler.fhir_client(session.id)
     fhir_practitioner = fhir_client.read(FHIR::Practitioner, params[:id]).resource
 
