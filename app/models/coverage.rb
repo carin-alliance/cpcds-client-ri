706f59bda569d050d8dc0203edc53940c0f61574
Vanessa Fotso <vfotso@mitre.org>
2022-09-28 21:02:02 -0400

    Display human readable text for coverage subscriber relationship

--- app/models/coverage.rb
+++ app/models/coverage.rb
@@ -10,7 +10,7 @@ class Coverage < Resource
 
 	include ActiveModel::Model
 
-  attr_reader :id, :identifiers, :payors, :period_start, :period_end, :relationship, :subscriberId, :type, :classes 
+  attr_reader :id, :identifiers, :payors, :period_start, :period_end, :relationship, :subscriberId, :type, :classes
 
   #-----------------------------------------------------------------------------
 
@@ -20,7 +20,7 @@ class Coverage < Resource
     @payors = get_payors(fhir_coverage.payor, organizations)
     @period_start = dateToString(fhir_coverage.period.start) if fhir_coverage.period
     @period_end = dateToString(fhir_coverage.period.end) if fhir_coverage.period
-    @relationship = codeable_concept_to_string(fhir_coverage.relationship)
+    @relationship = SUSCRIBER_RELATIONSHIP_CS[fhir_coverage.relationship.coding.first.code]
     @subscriberId = fhir_coverage.subscriberId
     @type = codeable_concept_to_string(fhir_coverage.type)
     @classes = get_classes(fhir_coverage.local_class)
@@ -30,12 +30,23 @@ class Coverage < Resource
     payor_ids = payor_refs.map { |payor| get_id_from_reference(payor.reference) }
     payor_ids.map { |id| elementwithid(organizations, id) }
   end
-  
+
   def get_classes(fhir_classes)
     fhir_classes.map do |c|
       Struct.new(*[:name, :value, :type])
             .new(*[c.name, c.value, codeable_concept_to_string(c.type)])
     end
   end
-  
+
+  private
+  SUSCRIBER_RELATIONSHIP_CS = {
+    "child" => "Child",
+    "parent" => "Parent",
+    "spouse" => "Spouse",
+    "common" => "Common Law Spouse",
+    "other" => "Other",
+    "self" => "Self",
+    "injured" => "Injured Party",
+  }
+
 end
