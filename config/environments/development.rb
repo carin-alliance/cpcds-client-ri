a667e465cb9c547b647b4958c506e771aacb057b
bstewartlg <114590619+bstewartlg@users.noreply.github.com>
2023-01-05 11:42:15 -0500

    Change allowed hosts for dev config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -75,4 +75,8 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  # Override rails 6+ dev default blocked host behavior
+  config.hosts.clear
+
 end
